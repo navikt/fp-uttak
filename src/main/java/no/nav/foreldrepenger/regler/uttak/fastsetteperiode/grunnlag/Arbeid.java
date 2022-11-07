938e0a397a116d1457b514954d4b6a20c5ce9a6e
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2022-11-07 22:53:29 +0100

    NOJIRA Ta med arbeid fra IAY-register pga utsettelse av tilkommet u/beregning

--- src/main/java/no/nav/foreldrepenger/regler/uttak/fastsetteperiode/grunnlag/Arbeid.java
+++ src/main/java/no/nav/foreldrepenger/regler/uttak/fastsetteperiode/grunnlag/Arbeid.java
@@ -8,20 +8,21 @@ import java.util.stream.Collectors;
 
 public final class Arbeid {
 
-    private final Set<Arbeidsforhold> arbeidsforholdListe = new HashSet<>();
+    private final Set<Arbeidsforhold> arbeidsforholdListe = new HashSet<>(); // Arbeidsforhold brukt i beregning, tilkommet dersom greadering eller refusjon
+    private final Set<Arbeidsforhold> arbeidsforholdFraRegister = new HashSet<>(); // Alle arbeidsforhold fra register
 
     private Arbeid() {
     }
 
     public BigDecimal getStillingsprosent(LocalDate dato) {
-        return arbeidsforholdListe.stream()
+        return arbeidsforholdFraRegister.stream()
                 .map(arbeidsforhold -> getStillingsprosent(dato, arbeidsforhold.getIdentifikator()))
                 .reduce(BigDecimal::add)
                 .orElse(BigDecimal.valueOf(100));
     }
 
     public BigDecimal getStillingsprosent(LocalDate dato, AktivitetIdentifikator aktivitet) {
-        var arbeidsforhold = arbeidsforholdListe.stream()
+        var arbeidsforhold = arbeidsforholdFraRegister.stream()
                 .filter(a -> a.getIdentifikator().equals(aktivitet))
                 .findFirst()
                 .orElseThrow();
@@ -39,12 +40,25 @@ public final class Arbeid {
         return arbeidsforholdListe;
     }
 
+    public Set<AktivitetIdentifikator> getRegisterAktiviteter() {
+        return arbeidsforholdFraRegister.stream().map(Arbeidsforhold::getIdentifikator).collect(Collectors.toSet());
+    }
+
+    public Set<Arbeidsforhold> getArbeidsforholdFraRegister() {
+        return arbeidsforholdFraRegister;
+    }
+
     public static class Builder {
 
         private final Arbeid kladd = new Arbeid();
 
         public Builder arbeidsforhold(Arbeidsforhold arbeidsforhold) {
             kladd.arbeidsforholdListe.add(arbeidsforhold);
+            return this;
+        }
+
+        public Builder arbeidsforholdFraRegister(Arbeidsforhold arbeidsforhold) {
+            kladd.arbeidsforholdFraRegister.add(arbeidsforhold);
             return this;
         }
 
