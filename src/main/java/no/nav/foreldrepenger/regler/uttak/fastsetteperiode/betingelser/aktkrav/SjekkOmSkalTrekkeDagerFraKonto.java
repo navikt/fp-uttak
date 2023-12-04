6c91d1ebe02207879e9e8e78a7399b8d7a4747ae
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2023-12-04 23:23:43 +0100

    TFP-5634 trekk dager ved avslag for sammenhengende+bfhr

---
+++ src/main/java/no/nav/foreldrepenger/regler/uttak/fastsetteperiode/betingelser/aktkrav/SjekkOmSkalTrekkeDagerFraKonto.java
@@ -0,0 +1,23 @@
+package no.nav.foreldrepenger.regler.uttak.fastsetteperiode.betingelser.aktkrav;
+
+import no.nav.foreldrepenger.regler.uttak.fastsetteperiode.FastsettePeriodeGrunnlag;
+import no.nav.fpsak.nare.doc.RuleDocumentation;
+import no.nav.fpsak.nare.evaluation.Evaluation;
+import no.nav.fpsak.nare.specification.LeafSpecification;
+
+@RuleDocumentation(SjekkOmSkalTrekkeDagerFraKonto.ID)
+public class SjekkOmSkalTrekkeDagerFraKonto extends LeafSpecification<FastsettePeriodeGrunnlag> {
+    public static final String ID = "AVSLAG_AKT_TREKK";
+    public static final String BESKRIVELSE = "Skal avslag trekke dager?";
+
+    public SjekkOmSkalTrekkeDagerFraKonto() {
+        super(ID);
+    }
+
+    @Override
+    public Evaluation evaluate(FastsettePeriodeGrunnlag fastsettePeriodeGrunnlag) {
+        var sammenhengendeUttak = fastsettePeriodeGrunnlag.kreverBehandlingSammenhengendeUttak();
+        var bareFarRett = fastsettePeriodeGrunnlag.isFarRett() && !fastsettePeriodeGrunnlag.isMorRett();
+        return sammenhengendeUttak || bareFarRett ? ja() : nei();
+    }
+}
